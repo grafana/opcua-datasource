c576cb5ee6c9a391f85fffb585152491cfc3ae7d
Christian Fjeld <christian_fjeld@hotmail.com>
2021-02-15 12:09:20 +0100

    changed appearance of event filter table

--- src/components/EventFilterTable.tsx
+++ src/components/EventFilterTable.tsx
@@ -5,10 +5,9 @@ import TableCell from '@material-ui/core/TableCell';
 //import TableHead from "@material-ui/core/TableHead";
 import TableRow from '@material-ui/core/TableRow';
 //import { withStyles, makeStyles } from '@material-ui/core/styles';
-import { Paper } from '@material-ui/core';
+import { Paper, TableHead } from '@material-ui/core';
 import { FilterOperator, EventFilter } from '../types';
 import { GrafanaTheme } from '@grafana/data';
-import { Button } from '@grafana/ui';
 import { EventFilterOperatorUtil } from '../utils/Operands';
 
 export interface Props {
@@ -23,6 +22,10 @@ type State = {
     nsTableFetched: boolean
 };
 
+const imageHeight: number = 20;
+const imageWidth: number = 20;
+
+
 export class EventFilterTable extends PureComponent<Props, State> {
   constructor(props: Props) {
       super(props);
@@ -34,24 +37,33 @@ export class EventFilterTable extends PureComponent<Props, State> {
     return (
       <TableRow style={{ height: 14 }} key={index}>
         <TableCell style={{ color: txt, border: 0, padding: 0 }}>
-          {' '}
-                {EventFilterOperatorUtil.GetOperandString(row.operands[0], this.state.nsTable)}{' '}
-        </TableCell>
-        <TableCell align="right" style={{ color: txt, border: 0, padding: 0 }}>
+          {EventFilterOperatorUtil.GetOperandString(row.operands[0], this.state.nsTable)}
+            </TableCell>
+        <TableCell style={{ color: txt, border: 0, padding: 0 }}>
           {EventFilterOperatorUtil.GetString(row.oper)}
         </TableCell>
         <TableCell style={{ color: txt, border: 0, padding: 0 }}>
-          {' '}
-                {EventFilterOperatorUtil.GetOperandString(row.operands[1], this.state.nsTable)}{' '}
+          {EventFilterOperatorUtil.GetOperandString(row.operands[1], this.state.nsTable)}
         </TableCell>
-        <TableCell>
-          <Button style={{ backgroundColor: bg }} onClick={() => this.props.ondelete(index)}>
-            Delete
-          </Button>
+        <TableCell style={{ color: txt, border: 0, padding: 0 }}>
+            {this.renderTrashBin(index)}
         </TableCell>
       </TableRow>
     );
-  }
+    }
+
+
+    renderTrashBin(index: number) {
+        // Courtesy of OG.
+        return (
+            <div style={{ display: 'inline-block', marginLeft: '5', cursor: 'pointer' }}>
+                <svg xmlns="http://www.w3.org/2000/svg" width={imageWidth} height={imageHeight} viewBox="0 0 24 24" fill="currentColor" onClick={() => this.props.ondelete(index)}>
+                    <path d="M10,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,10,18ZM20,6H16V5a3,3,0,0,0-3-3H11A3,3,0,0,0,8,5V6H4A1,1,0,0,0,4,8H5V19a3,3,0,0,0,3,3h8a3,3,0,0,0,3-3V8h1a1,1,0,0,0,0-2ZM10,5a1,1,0,0,1,1-1h2a1,1,0,0,1,1,1V6H10Zm7,14a1,1,0,0,1-1,1H8a1,1,0,0,1-1-1V8H17Zm-3-1a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,14,18Z">
+                    </path>
+                </svg>
+            </div>);
+
+    }
 
   renderDefaultRow(row: EventFilter, index: number, txt: string) {
     return (
@@ -88,7 +100,15 @@ export class EventFilterTable extends PureComponent<Props, State> {
     return (
       <div className="panel-container" style={{ width: '100' }}>
         <Paper>
-          <Table>
+            <Table>
+                <TableHead style={{ backgroundColor: bg, color: txt }}>
+                    <TableRow style={{ height: 20 }}>
+                        <TableCell style={{ color: txt, border: 0, padding: 2, whiteSpace: 'nowrap' }}>Operand</TableCell>
+                        <TableCell style={{ color: txt, border: 0, padding: 2, whiteSpace: 'nowrap' }}>Operator</TableCell>
+                        <TableCell style={{ color: txt, border: 0, padding: 2, whiteSpace: 'nowrap' }}>Operand</TableCell>
+                        <TableCell style={{ color: txt, border: 0, padding: 2, whiteSpace: 'nowrap' }}></TableCell>
+                    </TableRow>
+                </TableHead>
             <TableBody style={{ backgroundColor: bg, color: txt }}>
               {this.props.rows.map((row, index) => {
                 switch (row.oper) {
