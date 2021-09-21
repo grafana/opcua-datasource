c1e9ca806517462fc2c04045275266a13065fd0a
Stephanie Closson <srclosson@gmail.com>
2021-09-21 15:09:37 -0300

    simplify

--- scripts/ci-build-plugin.sh
+++ scripts/ci-build-plugin.sh
@@ -4,17 +4,12 @@
 
 # Build Linux
 dotnet publish ./backend/.linux.build.csproj -r linux-x64 --self-contained true
-# Lint
 mv -v dist ci/jobs/build_backend/linux
 
-# add dist back, plugin:ci-build --finish moved it
-mkdir dist
 # Build Windows
 dotnet publish ./backend/.windows.build.csproj -r win-x64 --self-contained true
-mv -v cdist ci/jobs/build_backend/windows
+mv -v dist ci/jobs/build_backend/windows
 
-# add dist back, plugin:ci-build --finish moved it
-mkdir dist
 # Build OSX
 dotnet publish ./backend/.osx.build.csproj -r osx-x64 --self-contained true
 mv -v dist ci/jobs/build_backend/darwin
