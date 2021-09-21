734d2a2b67335c69b44a091112007ee671b064d7
Stephanie Closson <srclosson@gmail.com>
2021-09-21 14:55:40 -0300

    update build script

--- scripts/ci-build-plugin.sh
+++ scripts/ci-build-plugin.sh
@@ -1,27 +1,20 @@
 #!/usr/bin/env sh
 
+[ ! -d ci/jobs/build_backend ] && mkdir -pv ci/jobs/build_backend
+
 # Build Linux
 dotnet publish ./backend/.linux.build.csproj -r linux-x64 --self-contained true
 # Lint
-./node_modules/.bin/grafana-toolkit plugin:ci-build --finish
-mv -v ci/jobs/build_backend/dist ci/jobs/build_backend_linux
+mv -v dist ci/jobs/build_backend/linux
 
 # add dist back, plugin:ci-build --finish moved it
 mkdir dist
 # Build Windows
 dotnet publish ./backend/.windows.build.csproj -r win-x64 --self-contained true
-./node_modules/.bin/grafana-toolkit plugin:ci-build --finish
-mv -v ci/jobs/build_backend/dist ci/jobs/build_backend_windows
+mv -v cdist ci/jobs/build_backend/windows
 
 # add dist back, plugin:ci-build --finish moved it
 mkdir dist
 # Build OSX
 dotnet publish ./backend/.osx.build.csproj -r osx-x64 --self-contained true
-./node_modules/.bin/grafana-toolkit plugin:ci-build --finish
-mv -v ci/jobs/build_backend/dist ci/jobs/build_backend_darwin
-
-[ ! -d ci/jobs/build_backend ] && mkdir -pv ci/jobs/build_backend
-# Organize results
-mv -v ci/jobs/build_backend_linux ci/jobs/build_backend/linux
-mv -v ci/jobs/build_backend_windows ci/jobs/build_backend/windows
-mv -v ci/jobs/build_backend_darwin ci/jobs/build_backend/darwin    
+mv -v dist ci/jobs/build_backend/darwin
