a21ebe1adb86da7440be91d8899b081404eb1177
Stephanie Closson <srclosson@gmail.com>
2022-02-09 11:37:49 -0400

    prettified

--- src/components/NodeQueryEditor.tsx
+++ src/components/NodeQueryEditor.tsx
@@ -92,30 +92,34 @@ export class NodeQueryEditor extends PureComponent<Props, State> {
 
   browse = (nodeId: string, browseFilter: BrowseFilter): Promise<OpcUaBrowseResults[]> => {
     var filter = JSON.stringify(browseFilter);
-      let res: Promise<OpcUaBrowseResults[]> = this.props.datasource.getResource('browse', { nodeId: nodeId, browseFilter: filter });
-      return res.then((children) => this.removeDuplicates(children));
+    let res: Promise<OpcUaBrowseResults[]> = this.props.datasource.getResource('browse', {
+      nodeId: nodeId,
+      browseFilter: filter,
+    });
+    return res.then((children) => this.removeDuplicates(children));
   };
 
   removeDuplicates(brRes: OpcUaBrowseResults[]): OpcUaBrowseResults[] {
     var encounteredSet = new Set();
     const uniqueBrs = brRes.filter((val) => {
-      if (encounteredSet.has(val.nodeId))
+      if (encounteredSet.has(val.nodeId)) {
         return false;
+      }
 
       encounteredSet.add(val.nodeId);
-        return true;
+      return true;
     });
     return uniqueBrs;
   }
 
   browseTypes = (nodeId: string, browseFilter: BrowseFilter): Promise<OpcUaBrowseResults[]> => {
     var filter = JSON.stringify(browseFilter);
-      let res: Promise<OpcUaBrowseResults[]> = this.props.datasource.getResource('browse', {
-        nodeId: nodeId,
-        nodeClassMask: NodeClass.ObjectType | NodeClass.VariableType,
-        browseFilter: filter,
-      });
-      return res.then((children) => this.removeDuplicates(children));
+    let res: Promise<OpcUaBrowseResults[]> = this.props.datasource.getResource('browse', {
+      nodeId: nodeId,
+      nodeClassMask: NodeClass.ObjectType | NodeClass.VariableType,
+      browseFilter: filter,
+    });
+    return res.then((children) => this.removeDuplicates(children));
   };
 
   getNodePath(nodeId: string, rootId: string): Promise<NodePath> {
