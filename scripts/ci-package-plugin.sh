81a78484117a3b9794b4a888f90d833827a30e08
Stephanie Closson <srclosson@gmail.com>
2023-10-21 16:31:19 -0300

    fix ci 14

--- scripts/ci-package-plugin.sh
+++ scripts/ci-package-plugin.sh
@@ -6,13 +6,13 @@ ci_package() {
   build="$1"
   arch="$2"
 
-  mkdir -pv ci/dist/grafana-opcua-datasource || true
-  cp -rfv ci/jobs/build-and-test-frontend/dist ci/dist/grafana-opcua-datasource
+  [ -d ci/dist/grafana-opcua-datasource ] && /bin/rm -rf ci/dist/grafana-opcua-datasource || echo "no dist dir found. Good."
   mv -v ci/jobs/build_backend/$build/dist ci/dist/grafana-opcua-datasource
-  pushd ci/dist
+  cp -rfv ci/jobs/build-and-test-frontend/dist/ ci/dist/grafana-opcua-datasource
+  cd ci/dist
   npx @grafana/sign-plugin@latest
   zip -r grafana-opcua-datasource_${build}_${arch}.zip grafana-opcua-datasource
-  popd
+  cd ../.. 
   mv ci/dist/grafana-opcua-datasource_${build}_${arch}.zip ci/packages
 }
 
