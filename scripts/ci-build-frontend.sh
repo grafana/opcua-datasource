d8631497af3cad9536687945290ed3856abf4a5d
Stephanie Closson <srclosson@gmail.com>
2023-10-21 13:44:19 -0300

    fix build 12

--- scripts/ci-build-frontend.sh
+++ scripts/ci-build-frontend.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-[ -d ci/jobs/build-and-test-frontend ] && /bin/rm -rf ci/jobs/build-and-test-frontend || echo "build-and-test-frontend already deleted"
+[ ! -d ci/jobs/build-and-test-frontend ] && mkdir -pv ci/jobs/build-and-test-frontend || echo "build-and-test-frontend already created"
 
 [ -d dist ] && /bin/rm -rf dist || echo "no dist directory exist"
 [ ! -d dist ] && mkdir -pv dist || echo "dist directory already created"
