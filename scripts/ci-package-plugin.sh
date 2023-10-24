14ef0e2afd9dd28b72c78ae30f4e04bdda9942e1
Stephanie Closson <srclosson@gmail.com>
2023-10-24 15:34:46 -0300

    fix build

--- scripts/ci-package-plugin.sh
+++ scripts/ci-package-plugin.sh
@@ -12,20 +12,17 @@ ci_package() {
   mv -v ci/jobs/build_backend/$build/dist ci/dist/grafana-opcua-datasource
   cp -rfv ci/jobs/build-and-test-frontend/dist/ ci/dist/grafana-opcua-datasource
 
-  PLUGIN_NAME=`cat ci/dist/plugin.json|jq '.id'| sed s/\"//g`
-  VERSION=`cat ci/dist/plugin.json|jq '.info.version'| sed s/\"//g`
+  PLUGIN_NAME=`cat ci/dist/grafana-opcua-datasource/plugin.json|jq '.id'| sed s/\"//g`
+  VERSION=`cat ci/dist/grafana-opcua-datasource/plugin.json|jq '.info.version'| sed s/\"//g`
   echo "Plugin Name: ${PLUGIN_NAME}"
   echo "Plugin Version: ${VERSION}"
   
   cd ci/dist/grafana-opcua-datasource
-  npx @grafana/sign-plugin@latest --rootUrls="http://localhost:3000"
+  npx @grafana/sign-plugin@latest 
   zip -r grafana-opcua-datasource_${build}_${arch}.zip grafana-opcua-datasource
   cd ../../.. 
   mv ci/dist/grafana-opcua-datasource_${build}_${arch}.zip ci/packages
 }
-
-
-
 
 ci_package "linux" "amd64"
 ci_package "windows" "amd64"
